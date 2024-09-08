9f21c07158c3e7c2004de4209294ecff28a75e38
iMoonDay <562613736@qq.com>
2024-09-09 01:28:31 +0800

    1.1.1

---
+++ common/src/main/java/com/imoonday/tradeenchantmentdisplay/config/ModConfig.java
@@ -0,0 +1,214 @@
+package com.imoonday.tradeenchantmentdisplay.config;
+
+import com.imoonday.tradeenchantmentdisplay.TradeEnchantmentDisplay;
+import com.imoonday.tradeenchantmentdisplay.renderer.EnchantmentRenderer;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
+import net.minecraft.core.Registry;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.world.item.Items;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Config(name = TradeEnchantmentDisplay.MOD_ID)
+public class ModConfig extends PartitioningSerializer.GlobalData {
+
+    @ConfigEntry.Category("screen")
+    @ConfigEntry.Gui.TransitiveObject
+    public Screen screen = new Screen();
+
+    @ConfigEntry.Category("hud")
+    @ConfigEntry.Gui.TransitiveObject
+    public Hud hud = new Hud();
+
+    @ConfigEntry.Category("merchant")
+    @ConfigEntry.Gui.TransitiveObject
+    public Merchant merchant = new Merchant();
+
+    @ConfigEntry.Category("generic")
+    @ConfigEntry.Gui.TransitiveObject
+    public Generic generic = new Generic();
+
+    @ConfigEntry.Category("cache")
+    @ConfigEntry.Gui.TransitiveObject
+    public Cache cache = new Cache();
+
+    public static ModConfig get() {
+        return AutoConfig.getConfigHolder(ModConfig.class).get();
+    }
+
+    public static Screen getScreen() {
+        return get().screen;
+    }
+
+    public static Hud getHud() {
+        return get().hud;
+    }
+
+    public static Merchant getMerchant() {
+        return get().merchant;
+    }
+
+    public static Generic getGeneric() {
+        return get().generic;
+    }
+
+    public static Cache getCache() {
+        return get().cache;
+    }
+
+    public static void save() {
+        AutoConfig.getConfigHolder(ModConfig.class).save();
+    }
+
+    @Config(name = "screen")
+    public static class Screen implements ConfigData {
+        public boolean enabled = true;
+        public boolean onlyEnchantedBooks = false;
+        public int offsetX = 82;
+        public int offsetY = 12;
+        public boolean xAxisCentered = true;
+        @ConfigEntry.BoundedDiscrete(min = 20, max = 200)
+        public int fontScale = 85;
+        @ConfigEntry.ColorPicker
+        public int fontColor = 0xFFFFFF;
+        @ConfigEntry.Gui.CollapsibleObject
+        public FontColorForMaxLevel fontColorForMaxLevel = new FontColorForMaxLevel();
+        @ConfigEntry.ColorPicker(allowAlpha = true)
+        public int bgColor = 0x00000000;
+        @ConfigEntry.BoundedDiscrete(max = 20 * 5)
+        @ConfigEntry.Gui.PrefixText
+        public int duration = 20;
+        public boolean displayOnTop = true;
+        @ConfigEntry.Gui.PrefixText
+        public String singularFormat = "$full_name";
+        @ConfigEntry.Gui.PrefixText
+        public String pluralFormat = "$full_name ($index/$total)";
+
+        @Override
+        public void validatePostLoad() throws ValidationException {
+            if (duration < 0) {
+                throw new ValidationException("Duration must be greater than or equal to 0");
+            }
+            if (pluralFormat.contains(EnchantmentRenderer.SIZE)) {
+                pluralFormat = pluralFormat.replace(EnchantmentRenderer.SIZE, EnchantmentRenderer.TOTAL);
+            }
+        }
+    }
+
+    @Config(name = "hud")
+    public static class Hud implements ConfigData {
+        public boolean enabled = true;
+        public boolean onlyEnchantedBooks = false;
+        public boolean tipForNoEnchantment = true;
+        public int offsetX = 10;
+        public int offsetY = 10;
+        @ConfigEntry.BoundedDiscrete(min = 20, max = 200)
+        public int scale = 100;
+        @ConfigEntry.ColorPicker
+        public int fontColor = 0xFFFFFF;
+        @ConfigEntry.Gui.CollapsibleObject
+        public FontColorForMaxLevel fontColorForMaxLevel = new FontColorForMaxLevel();
+        @ConfigEntry.ColorPicker(allowAlpha = true)
+        public int dividerColor = 0x50FFFFFF;
+        @ConfigEntry.ColorPicker(allowAlpha = true)
+        public int bgColor = 0x50000000;
+        @ConfigEntry.BoundedDiscrete(max = 20)
+        public int paddingX = 5;
+        @ConfigEntry.BoundedDiscrete(max = 20)
+        public int paddingY = 5;
+    }
+
+    @Config(name = "merchant")
+    public static class Merchant implements ConfigData {
+        public boolean enabled = true;
+        public double renderDistance = 64.0;
+        @ConfigEntry.BoundedDiscrete(max = 20 * 5)
+        @ConfigEntry.Gui.PrefixText
+        public int duration = 40;
+        public float offsetY = 0.0f;
+        @ConfigEntry.ColorPicker
+        public int nameColor = 0xFFAA00;
+        @ConfigEntry.Gui.CollapsibleObject
+        public FontColorForMaxLevel nameColorForMaxLevel = new FontColorForMaxLevel().withColor(0xFF5500);
+        @ConfigEntry.ColorPicker
+        public int priceColor = 0x55FF55;
+        public List<String> enchantmentBlacklist = new ArrayList<>();
+
+        @Override
+        public void validatePostLoad() throws ValidationException {
+            if (duration < 0) {
+                throw new ValidationException("Duration must be greater than or equal to 0");
+            }
+        }
+    }
+
+    @Config(name = "generic")
+    public static class Generic implements ConfigData {
+        public boolean alwaysAttemptToGetNearbyOffers = true;
+        public int requestFrequency = 5;
+        public List<String> nonInteractableItems = List.of(
+                Registry.ITEM.getKey(Items.VILLAGER_SPAWN_EGG).toString(),
+                Registry.ITEM.getKey(Items.NAME_TAG).toString()
+        );
+    }
+
+    @Config(name = "cache")
+    public static class Cache implements ConfigData {
+        public boolean enabled = true;
+        public String filePath = "trades.nbt";
+        public boolean distinguishPortBetweenServers = false;
+    }
+
+    public static class FontColorForMaxLevel {
+        public boolean enabled = true;
+        @ConfigEntry.ColorPicker
+        public int color = 0xFFD700;
+        public boolean bold = true;
+        public boolean italic = false;
+        public boolean underline = false;
+        public boolean ignoreSingleLevel = true;
+
+        public MutableComponent format(MutableComponent component) {
+            return component.setStyle(component.getStyle().withColor(color).withBold(bold).withItalic(italic).withUnderlined(underline));
+        }
+
+        public boolean shouldFormat(int level, int maxLevel) {
+            return enabled && level >= maxLevel && (!ignoreSingleLevel || maxLevel > 1 || level > 1);
+        }
+
+        public FontColorForMaxLevel withEnabled(boolean enabled) {
+            this.enabled = enabled;
+            return this;
+        }
+
+        public FontColorForMaxLevel withColor(int color) {
+            this.color = color;
+            return this;
+        }
+
+        public FontColorForMaxLevel withBold(boolean bold) {
+            this.bold = bold;
+            return this;
+        }
+
+        public FontColorForMaxLevel withItalic(boolean italic) {
+            this.italic = italic;
+            return this;
+        }
+
+        public FontColorForMaxLevel withUnderline(boolean underline) {
+            this.underline = underline;
+            return this;
+        }
+
+        public FontColorForMaxLevel withIgnoreSingleLevel(boolean ignoreSingleLevel) {
+            this.ignoreSingleLevel = ignoreSingleLevel;
+            return this;
+        }
+    }
+}
---
+++ common/src/main/java/com/imoonday/tradeenchantmentdisplay/util/MerchantOfferHandler.java
@@ -0,0 +1,135 @@
+package com.imoonday.tradeenchantmentdisplay.util;
+
+import com.imoonday.tradeenchantmentdisplay.TradeEnchantmentDisplay;
+import com.imoonday.tradeenchantmentdisplay.config.ModConfig;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.protocol.game.ServerboundInteractPacket;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.npc.AbstractVillager;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
+
+public class MerchantOfferHandler {
+
+    public static final int MAX_WAITING_TIME = 10;
+    private static int waitingTime = 0;
+
+    public static void clientWorldTick() {
+        updateWaitingTime();
+        if (!MerchantOfferUtils.shouldRequestingOffers()) return;
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.screen != null) return;
+        AbstractVillager merchant = getValidMerchant(mc);
+        MerchantOfferInfo info = MerchantOfferInfo.getInstance();
+        if (merchant != null && !isWaiting()) {
+            if (info.hasId(merchant.getId())) return;
+            info.clearOffers();
+            info.setId(merchant.getId());
+            sendRequest(merchant);
+        } else if (!TradeEnchantmentDisplay.isTrading()) {
+            info.clearId();
+        }
+    }
+
+    public static boolean isWaiting() {
+        return waitingTime > 0;
+    }
+
+    public static void startWaiting() {
+        startWaiting(MAX_WAITING_TIME);
+    }
+
+    public static void startWaiting(int time) {
+        waitingTime = time;
+    }
+
+    public static void updateWaitingTime() {
+        if (waitingTime > 0) {
+            waitingTime--;
+        }
+    }
+
+    public static AbstractVillager getValidMerchant(Minecraft mc) {
+        if (mc == null) return null;
+        LocalPlayer player = mc.player;
+        if (player == null) return null;
+        if (TradeEnchantmentDisplay.isTrading()) return null;
+        HitResult hitResult = mc.hitResult;
+        if (!(hitResult instanceof EntityHitResult entityHitResult) || entityHitResult.getType() == EntityHitResult.Type.MISS) {
+            return null;
+        }
+        Entity entity = entityHitResult.getEntity();
+        if (!(entity instanceof AbstractVillager merchant)) return null;
+        if (merchant instanceof Villager villager) {
+            VillagerProfession profession = villager.getVillagerData().getProfession();
+            if (profession == VillagerProfession.NONE || profession == VillagerProfession.NITWIT) {
+                return null;
+            }
+            ItemStack stack = player.getMainHandItem();
+            if (MerchantOfferUtils.checkInteractableWithVillager(stack)) {
+                startWaiting();
+                return null;
+            }
+        }
+        return merchant;
+    }
+
+    public static void clientTick(Minecraft mc) {
+        LocalPlayer player = mc.player;
+        MultiPlayerGameMode gameMode = mc.gameMode;
+        if (player != null && mc.level != null && gameMode != null) {
+            boolean tryInteract = ModConfig.getGeneric().alwaysAttemptToGetNearbyOffers && MerchantOfferUtils.shouldRequestingOffers();
+            if (tryInteract) {
+                MerchantOfferCache cache = MerchantOfferCache.getInstance();
+                List<Entity> entities = mc.level.getEntities(player, player.getBoundingBox().inflate(gameMode.getPickRange()));
+                entities.sort(Comparator.comparingDouble(e -> e.distanceTo(player)));
+                int cooldown = ModConfig.getGeneric().requestFrequency;
+                for (Entity entity : entities) {
+                    if (!cache.contains(entity.getUUID()) && MerchantOfferUtils.isValidMerchant(entity) && !MerchantOfferCache.isRequested(entity) && MerchantOfferUtils.tryRequest(entity)) {
+                        MerchantOfferCache.markRequested(entity);
+                        startWaiting(cooldown);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void onEntityRemoved(Entity entity) {
+        if (!(entity instanceof AbstractVillager)) return;
+        if (entity.isAlive()) return;
+        MerchantOfferCache cache = MerchantOfferCache.getInstance();
+        UUID uuid = entity.getUUID();
+        cache.removeIfExist(uuid);
+    }
+
+    public static boolean sendRequest(AbstractVillager merchant) {
+        ClientPacketListener connection = Minecraft.getInstance().getConnection();
+        if (connection != null) {
+            connection.send(ServerboundInteractPacket.createInteractionPacket(merchant, Minecraft.getInstance().player.isShiftKeyDown(), InteractionHand.MAIN_HAND));
+            return true;
+        }
+        return false;
+    }
+
+    public static void onJoined() {
+        MerchantOfferCache.getInstance().load();
+    }
+
+    public static void onDisconnected() {
+        MerchantOfferCache.getInstance().save();
+        MerchantOfferCache.getInstance().clear();
+        MerchantOfferCache.clearRequestedIds();
+    }
+}
