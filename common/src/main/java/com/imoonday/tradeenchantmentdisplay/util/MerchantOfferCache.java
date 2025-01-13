781c2763969dde945e9d3a5e7668227004bb7676
iMoonDay <562613736@qq.com>
2025-01-13 20:30:31 +0800

    1.1.5

--- common/src/main/java/com/imoonday/tradeenchantmentdisplay/util/MerchantOfferCache.java
+++ common/src/main/java/com/imoonday/tradeenchantmentdisplay/util/MerchantOfferCache.java
@@ -26,6 +26,9 @@ public class MerchantOfferCache {
     private static final Set<UUID> REQUESTED_IDS = new HashSet<>();
     private static File cacheFile;
     private final Map<UUID, MerchantOfferInfo> cache = new HashMap<>();
+    private static final long DEBOUNCE_DELAY = 1000;
+    private static int errorCount = 0;
+    private Timer timer;
 
     public static MerchantOfferCache getInstance() {
         return INSTANCE;
@@ -42,13 +45,13 @@ public class MerchantOfferCache {
     public boolean set(UUID uuid, MerchantOfferInfo info) {
         boolean contains = cache.containsKey(uuid);
         cache.put(uuid, info);
-        save();
+        saveWithDebounce();
         return contains;
     }
 
     public void remove(UUID uuid) {
         cache.remove(uuid);
-        save();
+        saveWithDebounce();
         unmarkRequested(uuid);
     }
 
@@ -60,101 +63,75 @@ public class MerchantOfferCache {
         return false;
     }
 
-    public void clear() {
-        cache.clear();
-        save();
-        clearRequestedIds();
+    public void saveWithDebounce() {
+        if (timer != null) {
+            timer.cancel();
+        }
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                save();
+            }
+        }, DEBOUNCE_DELAY);
     }
 
     public boolean contains(UUID uuid) {
         return cache.containsKey(uuid);
     }
 
-    public void update(UUID uuid, MerchantOfferInfo info) {
-        MerchantOfferInfo oldInfo = get(uuid);
-        if (oldInfo != null) {
-            oldInfo.update(info);
-            save();
-        } else {
-            set(uuid, info);
-        }
-    }
-
     public void save() {
-        ClientLevel level = Minecraft.getInstance().level;
-        if (level == null) return;
-        if (!ModConfig.getCache().enabled) return;
-        String name = getCurrentWorldName();
-        if (name == null) {
-            LOGGER.warn("Failed to get current world name");
-            return;
-        }
-        CompoundTag uuids = new CompoundTag();
-        cache.forEach((uuid, info) -> uuids.put(uuid.toString(), MerchantOffers.CODEC.encodeStart(level.registryAccess().createSerializationContext(NbtOps.INSTANCE), info.createOffers()).getOrThrow()));
-        CompoundTag root = new CompoundTag();
-        root.put(name, uuids);
         try {
-            File file = getCacheFile();
-            if (!file.exists() && !file.createNewFile()) {
-                LOGGER.warn("Failed to create cache file");
-                return;
-            }
-            CompoundTag oldCache;
+            ClientLevel level = Minecraft.getInstance().level;
+            if (level == null) return;
+            if (!ModConfig.getCache().enabled) return;
+            String name = getCurrentWorldName();
+            if (name == null) return;
+
+            CompoundTag uuids = new CompoundTag();
+            cache.forEach((uuid, info) -> uuids.put(uuid.toString(), MerchantOffers.CODEC.encodeStart(level.registryAccess().createSerializationContext(NbtOps.INSTANCE), info.createOffers()).getOrThrow()));
+            CompoundTag root = new CompoundTag();
+            root.put(name, uuids);
             try {
-                oldCache = NbtIo.readCompressed(file.toPath(), NbtAccounter.unlimitedHeap());
-                oldCache.remove(name);
+                File file = getCacheFile();
+                if (!file.exists() && !file.createNewFile()) {
+                    LOGGER.warn("Failed to create cache file");
+                    return;
+                }
+                CompoundTag oldCache;
+                try {
+                    oldCache = NbtIo.readCompressed(file.toPath(), NbtAccounter.unlimitedHeap());
+                    oldCache.remove(name);
+                } catch (IOException e) {
+                    oldCache = new CompoundTag();
+                }
+                oldCache.merge(root);
+                NbtIo.writeCompressed(oldCache, file.toPath());
+                errorCount = 0;
             } catch (IOException e) {
-                oldCache = new CompoundTag();
+                LOGGER.error("Failed to save cache", e);
             }
-            oldCache.merge(root);
-            NbtIo.writeCompressed(oldCache, file.toPath());
-        } catch (IOException e) {
-            LOGGER.error("Failed to save cache", e);
+        } catch (Exception e) {
+            LOGGER.error("Error while saving cache, stopping saving. If this error persist, please try to delete cache file {} and report this issue", getCacheFile(), e);
+            handleError();
         }
     }
 
-    public void load() {
-        ClientLevel level = Minecraft.getInstance().level;
-        if (level == null) return;
-        if (!ModConfig.getCache().enabled) return;
-        LOGGER.info("Loading cache");
-        String name = getCurrentWorldName();
-        if (name == null) {
-            LOGGER.warn("Failed to get current world name");
-            return;
+    private static void handleError() {
+        if (++errorCount > 10) {
+            errorCount = 0;
+            LOGGER.error("Too many errors while saving or loading cache, disabling cache");
+            ModConfig.getCache().enabled = false;
+            ModConfig.save();
         }
-        CompoundTag root;
-        File file = getCacheFile();
-        if (!file.exists()) {
-            LOGGER.warn("No cache file exist");
-            return;
-        }
-        try {
-            root = NbtIo.readCompressed(file.toPath(), NbtAccounter.unlimitedHeap());
-        } catch (IOException e) {
-            LOGGER.error("Failed to read cache file", e);
-            return;
-        }
-        if (!root.contains(name)) {
-            LOGGER.info("No cache for current world");
-            return;
-        }
-        CompoundTag uuids = root.getCompound(name);
-        uuids.getAllKeys().forEach(key -> {
-            CompoundTag offers = uuids.getCompound(key);
-            MerchantOffers.CODEC.parse(level.registryAccess().createSerializationContext(NbtOps.INSTANCE), offers).resultOrPartial(Util.prefix("Failed to load offers: ", LOGGER::warn)).ifPresent(merchantOffers -> {
-                MerchantOfferInfo info = new MerchantOfferInfo(merchantOffers);
-                cache.put(UUID.fromString(key), info);
-            });
-        });
-        LOGGER.info("Cache loaded");
     }
 
     @Nullable
     public static String getCurrentWorldName() {
         String name = null;
-        Minecraft mc = Minecraft.getInstance();
         try {
+            Minecraft mc = Minecraft.getInstance();
             if (mc.hasSingleplayerServer()) {
                 name = ((MinecraftServerAccessor) mc.getSingleplayerServer()).getStorageSource().getLevelId();
             } else {
@@ -164,9 +141,80 @@ public class MerchantOfferCache {
                 }
             }
         } catch (Exception e) {
-            return null;
+            LOGGER.warn("Failed to get current world name", e);
         }
         return name;
+    }
+
+    public void clear() {
+        cache.clear();
+        saveWithDebounce();
+        clearRequestedIds();
+    }
+
+    public void update(UUID uuid, MerchantOfferInfo info) {
+        MerchantOfferInfo oldInfo = get(uuid);
+        if (oldInfo != null) {
+            oldInfo.update(info);
+            saveWithDebounce();
+        } else {
+            set(uuid, info);
+        }
+    }
+
+    public void load() {
+        try {
+            ClientLevel level = Minecraft.getInstance().level;
+            if (level == null) return;
+            if (!ModConfig.getCache().enabled) return;
+            LOGGER.info("Loading cache");
+            String name = getCurrentWorldName();
+            if (name == null) return;
+
+            CompoundTag root;
+            File file = getCacheFile();
+            if (!file.exists()) {
+                LOGGER.warn("No cache file exist");
+                return;
+            }
+            try {
+                root = NbtIo.readCompressed(file.toPath(), NbtAccounter.unlimitedHeap());
+            } catch (IOException e) {
+                LOGGER.error("Failed to read cache file", e);
+                return;
+            }
+            if (!root.contains(name)) {
+                LOGGER.info("No cache for current world");
+                return;
+            }
+
+            int count = 0;
+
+            CompoundTag uuids = root.getCompound(name);
+            for (String key : uuids.getAllKeys()) {
+                CompoundTag offers = uuids.getCompound(key);
+                Optional<MerchantOffers> optional = MerchantOffers.CODEC.parse(level.registryAccess().createSerializationContext(NbtOps.INSTANCE), offers).resultOrPartial(Util.prefix("Failed to load offers: ", LOGGER::warn));
+                if (optional.isPresent()) {
+                    MerchantOffers merchantOffers = optional.get();
+                    MerchantOfferInfo info = new MerchantOfferInfo(merchantOffers);
+                    UUID uuid;
+                    try {
+                        uuid = UUID.fromString(key);
+                    } catch (IllegalArgumentException e) {
+                        LOGGER.error("Failed to parse UUID", e);
+                        continue;
+                    }
+                    cache.put(uuid, info);
+                    count++;
+                }
+            }
+
+            errorCount = 0;
+            LOGGER.info("Loaded {} offers from cache", count);
+        } catch (Exception e) {
+            LOGGER.error("Error while loading cache, stopping loading. If this error persist, please try to delete cache file {} and report this issue", getCacheFile(), e);
+            handleError();
+        }
     }
 
     private static File getCacheFile() {
